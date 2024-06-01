a1547831bf817f263df63bcd13ab42f23e76eecd
EtherealDivine <paprika1440@gmail.com>
2024-06-01 21:29:36 +0000

    update README.md

--- myApp.js
+++ myApp.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
+
+const PORT = process.env.PORT || 3030;
+
+
+app.listen(PORT, () => {
+  console.log(`☕ Server started on port ${PORT}`);
+});
 
 
 
